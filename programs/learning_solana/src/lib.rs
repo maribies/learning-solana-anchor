a3cbd06c589ab0dc1f8d5ea4fe566753b7ee2874
Marissa Biescker <marissa.biesecker@gmail.com>
2022-07-21 18:36:28 +0200

    Add accomplishments struct to account

--- programs/learning_solana/src/lib.rs
+++ programs/learning_solana/src/lib.rs
@@ -13,3 +13,10 @@ pub mod learning_solana {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+// account struct https://book.anchor-lang.com/anchor_in_depth/the_accounts_struct.html
+#[account]
+// Struct is pretty straightforward
+pub struct Accomplishments {
+    pub cadet_mastery: Vec<String>,
+}
