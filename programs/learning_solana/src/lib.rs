80a95fcabe96b224b5378bd0a6b986e095e22130
Marissa Biescker <marissa.biesecker@gmail.com>
2022-07-22 12:07:13 +0200

    Add comments for understanding and context

--- programs/learning_solana/src/lib.rs
+++ programs/learning_solana/src/lib.rs
@@ -2,10 +2,14 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+// macro- sharable extraction of code at syntax level
 #[program]
+// module - a structure to organize code and control scope
 pub mod learning_solana {
     use super::*;
 
+    // initialization function, which is public to be called
+    // from the outside to run the program.
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
